d355bc2eab5f3cf791096120a1a293fb19bd710e
Valtkf <valentin.vgregorio@gmail.com>
2024-07-18 17:54:46 +0200

    📦 F01 : Add Header Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <h1 className="bg-red-800 text-3xl font-bold underline">Hello world!</h1>
+    <div className="bg-stone-900 h-screen">
+      <Header />
+      <h1 className="bg-red-800 text-3xl font-bold underline mt-[96px]">
+        Hello world!
+      </h1>
+    </div>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+  return (
+    <div className="h-[96px] border-2 border-rose-500 text-white text-center font-sans pt-[24xp] py-6">
+      <a href="" className="p-4">
+        Header
+      </a>
+      <a href="" className="p-4">
+        About
+      </a>
+      <a href="" className="p-4">
+        Projects
+      </a>
+      <a href="" className="p-4">
+        Contact
+      </a>
+    </div>
+  );
+}
