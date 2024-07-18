17009167594208bcb0b4682e7a79116c352cdeda
Valtkf <valentin.vgregorio@gmail.com>
2024-07-18 18:33:45 +0200

    ✨  F01 : Header > Add social links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,39 @@
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+
 export default function Header() {
   return (
-    <div className="h-[96px] border-2 border-rose-500 text-white text-center font-sans pt-[24xp] py-6">
-      <a href="" className="p-4">
-        Header
-      </a>
-      <a href="" className="p-4">
-        About
-      </a>
-      <a href="" className="p-4">
-        Projects
-      </a>
-      <a href="" className="p-4">
-        Contact
-      </a>
+    <div className="text-base font-normal h-[96px] border-2 border-rose-500 text-white text-center font-sans pt-6 flex justify-around items-center px-4">
+      <div className=" font-poppins ">
+        <a href="" className="p-4">
+          Header
+        </a>
+        <a href="" className="p-4">
+          About
+        </a>
+        <a href="" className="p-4">
+          Projects
+        </a>
+        <a href="" className="p-4">
+          Contact
+        </a>
+      </div>
+      <div className="flex items-center gap-6">
+        <a
+          href="https://www.linkedin.com/in/valentin-gregorio-33510826a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AiOutlineLinkedin />
+        </a>
+        <a
+          href="https://github.com/Valtkf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </div>
     </div>
   );
 }
