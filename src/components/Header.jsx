f8ab978436a40210347f55c68273035373d9a8b7
Valtkf <valentin.vgregorio@gmail.com>
2024-07-22 01:21:53 +0200

    F01 : Add responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,11 +17,11 @@ export default function Header() {
   };
 
   return (
-    <div className="m-auto z-10 fixed top-0 left-0 w-full overflow-hidden bg-stone-900 text-white p-4 flex justify-center items-center h-20">
-      <div className="text-2xl rounded-full border-[1px] border-stone-500 p-2 h-full flex items-center justify-center">
+    <div className="m-auto z-10 fixed top-0 left-0 w-full overflow-hidden bg-stone-900 text-white p-2 md:p-4 flex justify-center items-center h-16 md:h-20">
+      <div className="text-xl md:text-2xl rounded-full border-[1px] border-stone-500 p-1 md:p-2 h-full flex items-center justify-center">
         <motion.a
           href="#header"
-          className={`pr-4 pl-4 ${
+          className={`px-2 md:px-4 ${
             activeTab === "header" ? "bg-stone-700 rounded-xl" : ""
           }`}
           onClick={() => handleClick("header")}
@@ -29,14 +29,17 @@ export default function Header() {
           whileTap={{ scale: 0.9 }}
           style={{
             transition: "background-color 0.4s ease",
-            fontSize: "1.25rem",
+            fontSize: "1rem", // Taille de police réduite pour les petits écrans
+            md: {
+              fontSize: "1.25rem", // Taille de police normale pour les écrans moyens et plus
+            },
           }}
         >
           Header
         </motion.a>
         <motion.a
           href="#about"
-          className={`pr-4 pl-4 ${
+          className={`px-2 md:px-4 ${
             activeTab === "about" ? "bg-stone-700 rounded-xl" : ""
           }`}
           onClick={() => handleClick("about")}
@@ -44,14 +47,17 @@ export default function Header() {
           whileTap={{ scale: 0.9 }}
           style={{
             transition: "background-color 0.4s ease",
-            fontSize: "1.25rem",
+            fontSize: "1rem",
+            md: {
+              fontSize: "1.25rem",
+            },
           }}
         >
           About
         </motion.a>
         <motion.a
           href="#projects"
-          className={`pr-4 pl-4 ${
+          className={`px-2 md:px-4 ${
             activeTab === "projects" ? "bg-stone-700 rounded-xl" : ""
           }`}
           onClick={() => handleClick("projects")}
@@ -59,14 +65,17 @@ export default function Header() {
           whileTap={{ scale: 0.9 }}
           style={{
             transition: "background-color 0.4s ease",
-            fontSize: "1.25rem",
+            fontSize: "1rem",
+            md: {
+              fontSize: "1.25rem",
+            },
           }}
         >
           Projects
         </motion.a>
         <motion.a
           href="#contact"
-          className={`pr-4 pl-4 ${
+          className={`px-2 md:px-4 ${
             activeTab === "contact" ? "bg-stone-700 rounded-xl" : ""
           }`}
           onClick={() => handleClick("contact")}
@@ -74,7 +83,10 @@ export default function Header() {
           whileTap={{ scale: 0.9 }}
           style={{
             transition: "background-color 0.4s ease",
-            fontSize: "1.25rem",
+            fontSize: "1rem",
+            md: {
+              fontSize: "1.25rem",
+            },
           }}
         >
           Contact
